21531e2bd6419ca6833e3a583f1304b39a9142dc
Nicolas <nico@dassas-henny.com>
2026-03-13 00:17:38 +0100

    implementing substitution

--- lib/Terms.hs
+++ lib/Terms.hs
@@ -1,9 +1,15 @@
 module Terms where
 
-data Term = Var String | Function String [Term] -- A term is either a variable or a function (a 0-ary function being a constant)
+import Data.Map (Map)
+
+type Variable = String -- A variable is just defined by its name
+
+data Term = Var Variable | Function String [Term] -- A term is either a variable or a function (a 0-ary function being a constant)
 
 data Clause = Fact Term | Rule Term [Term] -- A Clause is either an axiom or a conclusion and a set of premisses
 
 type Program = [Clause] -- A program is a set of clauses
 
 type Query = [Term] -- A query is a set of terms, interpreted as a conjunction 
+
+type Subst = Map Variable Term -- A substitution associates a term to a variable
--- lib/Unify.hs
+++ lib/Unify.hs
@@ -1,4 +1,12 @@
 module Unify where
 
+import qualified Data.Map as Map
 import Terms
 
+applySubst :: Subst -> Term -> Term
+applySubst s (Var x) =
+  case Map.lookup x s of
+    Nothing -> Var x
+    Just t -> applySubst s t -- we propagate the substitution
+applySubst s (Function name l) =
+  Function name (map (applySubst s) l)
