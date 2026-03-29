d2600041b012d38ed1bd4ca314f324a30eeec438
Dionysis <dionysis.yusofy@gmail.com>
2026-03-29 11:38:50 +0200

    added source

--- examples/biggerRules.pl
+++ examples/biggerRules.pl
@@ -1,5 +1,7 @@
-bigger(cat(), mouse()):-
-bigger(dog(), cat()):-
-bigger(horse(), dog()):-
+bigger(cat(), mouse()):-.
+bigger(dog(), cat()):-.
+bigger(horse(), dog()):-.
 is_bigger(A,B):- bigger(A,B)
-is_bigger(A,B):- bigger(A,Z) & is_bigger(Z,B)
+is_bigger(A,B):- bigger(A,Z) & is_bigger(Z,B).
+
+% Example from here: https://swish.swi-prolog.org/p/introduction_to_logic_and_programming.swinb
